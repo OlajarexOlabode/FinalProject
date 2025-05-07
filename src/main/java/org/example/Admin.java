e220ce6aeeab3439369e815ded2c16c23a7d4ef6
olajireolabode <98364070+olajireolabode@users.noreply.github.com>
2025-05-07 10:32:48 -0400

    Admin class code & more

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -11,7 +11,9 @@ public class Admin extends User {
      */
     @Override
     public void viewMenu(Menu menu) {
-
+        System.out.println("Admin - Menu: ");
+        menu.getItems()
+                .forEach(item -> System.out.printf("- %s: $%.2f", item.getName(), item.getPrice()));
     }
 
     /**
@@ -20,7 +22,12 @@ public class Admin extends User {
      * @param item the item to be added
      */
     public void addFoodItem(Menu menu, FoodItem item) {
-
+        if (item != null && menu != null) {
+            menu.addItem(item);
+            System.out.println(item.getName() + " added.");
+        } else {
+            System.out.println("No item/menu.");
+        }
     }
 
     /**
@@ -29,6 +36,11 @@ public class Admin extends User {
      * @param itemName the item to be removed
      */
     public void removeFoodItem(Menu menu, String itemName) {
-
+        if (itemName != null && itemName.isBlank()) { //isBlank for empty/whitespaces
+            menu.removeItem(itemName);
+            System.out.println(itemName + " removed.");
+        } else {
+            System.out.println("Invalid item name.");
+        }
     }
 }
